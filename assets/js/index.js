bd416a9bebd1ec628d2b774c0a57fca046c0e6a2
Jane <nonstop00066@gmail.com>
2022-06-24 16:06:02 +0300

    Add methods examples

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,3 +24,11 @@ const btns1 = document.getElementsByClassName('btn');
 const btns2 = document.querySelectorAll('.btn');
 
 //btn.addEventListener('click', btnHandler);
+
+const ul = document.getElementsByTagName('ul')[0];
+// const ul = document.querySelector('h1+nav>ul');
+const li = document.querySelector('li:nth-child(4)');
+const h2 = document.getElementsByTagName('h2');
+// const h2 = document.querySelector('h2');
+const img = document.querySelector('img[alt=sea]');
+const p = document.querySelector('img[alt=sea]+.text');
